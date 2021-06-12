9124ecb1f819da0d3fa021b5a06200963fe5ea0d
Sam <sam.huang852@gmail.com>
2021-06-12 18:54:55 +1000

    Cat animations idle and up done
    Waiting on down animation

---
+++ Assets/Scripts/Controller/PayloadAnimationController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayloadAnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private Rigidbody2D _rb;
+    private static readonly int MoveUp = Animator.StringToHash("MoveUp");
+    private static readonly int MoveDown = Animator.StringToHash("MoveDown");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        //_animator.SetFloat("Y", moveY);
+        if (_rb.velocity.y > 1f)
+        {
+            _animator.SetBool(MoveUp, true);
+            _animator.SetBool(MoveDown, false);
+        }
+        else if (_rb.velocity.y < -1f)
+        {
+            _animator.SetBool(MoveDown, true);
+            _animator.SetBool(MoveUp, false);
+        }
+        else
+        {
+            _animator.SetBool(MoveDown, false);
+            _animator.SetBool(MoveUp, false);
+        }
+    }
+}
